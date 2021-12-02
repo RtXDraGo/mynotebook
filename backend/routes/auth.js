2d27d03083d4591fff01ce7e26154f26a1170eae
RtXDraGo <amisurjo634@gmail.com>
2021-12-02 14:50:12 +0530

    fourth commit

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,8 +2,11 @@ const express=require('express');
 const User=require('../models/User')//Initialize user from models-User
 const { body, validationResult } = require('express-validator');//this is needed for validatin check
 const router=express.Router();//intialize router to route
+const bcrypt = require('bcryptjs');
+var jwt = require('jsonwebtoken');
+const JWT_SECRET='I am a good boy'
 //create a user using /api/auth/createuser and check for validation
-router.post('/createuser',[body('email').isEmail(),body('name','Enter a valid name').isLength({ min: 3 }),  body('password','Enter a valid password').isLength({ min: 5 })],(req,res)=>{
+router.post('/createuser',[body('email').isEmail(),body('name','Enter a valid name').isLength({ min: 3 }),  body('password','Enter a valid password').isLength({ min: 5 })],async (req,res)=>{
     //If there are errors return those errors
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
@@ -11,17 +14,30 @@ router.post('/createuser',[body('email').isEmail(),body('name','Enter a valid na
     }
     try {
         //if no errors initialize name,email,password save to database
-    User.create({
+        const salt=await bcrypt.genSalt(10);
+        secPass=await bcrypt.hash(req.body.password,salt)
+        let user=await User.findOne({email:req.body.email});
+        if(user)
+        {
+          return res.status(400).json({error:"Emails should be unique"})
+        }
+     user=await User.create({
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password,
-      }).then(user => res.json(user))
-      .catch(err=>{console.log(err)
-      res.json({error:'please enter unique emailid'})})//if duplicate email id is present
+        password: secPass,
+      })
+      const data={
+          user:{
+              id:user.id
+          }
+      }
+      const jwdata=jwt.sign(data,JWT_SECRET);
+      console.log(jwdata)
+      res.json(user)
+      //if duplicate email id is present
     } catch (error) {
      console.error(error.message);   
      res.status(500).send("Some error occured");//if some other errors occures
     }
 })
-
 module.exports=router
